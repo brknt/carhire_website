ab64fca4481bdadfe16956cf450edefc9af855ff
brknt <brknt.gns@hotmail.com>
2019-01-12 22:13:48 +0300

    include all update(logib)

--- api/User/signup.php
+++ api/User/signup.php
@@ -12,31 +12,28 @@ $db = $database->getConnection();
 $user = new User($db);
  
 // set user property values
-
 $user->username = $_POST['username'];
 $user->password = base64_encode($_POST['password']);
 $user->created = date('Y-m-d H:i:s');
-
- if(($user->username!=""&&$user->password!=""&&$user->created!="") && $user->signup()){
-	 // create the user,
-	 //kaydedilirse yapılacaklar
-$user_arr=array(
+ 
+// create the user
+//kaydedilirse yapılacaklar
+if($user->signup()){
+    $user_arr=array(
         "status" => true,
         "message" => "Successfully Signup!",
         "id" => $user->id,
         "username" => $user->username
     );
+	
+	header("Location=index.html");
 }
-
 else{
-	//kaydedilmezse yapılacaklar
-	$user_arr=array(
+    $user_arr=array(
         "status" => false,
         "message" => "Username already exists!"
     );
+	
 }
- 
-
-
 print_r(json_encode($user_arr));
 ?>
